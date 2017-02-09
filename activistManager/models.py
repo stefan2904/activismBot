1150783fb32360594ebe39f27baf62bf71c51748
stefan <dev+git@2904.cc>
2017-02-09 02:47:55 +0100

    added bot reference to activist model

--- activistManager/models.py
+++ activistManager/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+from botManager.models import Bot
+
 
 class Activist(models.Model):
     identifier = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     reg_date = models.DateTimeField('Date registered')
+    bot = models.ForeignKey(Bot)  # on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} ({}: {})'.format(self.name, self.bot.name, self.identifier)
